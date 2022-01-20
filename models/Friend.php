522425f6bf90d5381e4295c8985b6200813f7bea
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-01-20 19:00:13 +0800

    :sparkles: Add provision to delete friend requests

--- models/Friend.php
+++ models/Friend.php
@@ -29,4 +29,14 @@ class Friend extends User {
 		$this->db->bind(3, $to);
 		return $this->db->execute();
 	}
+
+	public function deleteFriendRequest($from, $to) {
+		$this->db->query("
+			DELETE FROM `friend`
+			WHERE `from` = ? AND `to` = ?
+		");
+		$this->db->bind(1, $from);
+		$this->db->bind(2, $to);
+		return $this->db->execute();
+	}
 }
