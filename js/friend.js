57d47b23bf4add8d7219139585da052270fc2a81
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-01-25 16:46:03 +0800

    :ambulance: Make adjustments due to API call migration

--- js/friend.js
+++ js/friend.js
@@ -1,8 +1,11 @@
 // jshint esversion: 6
 function displayWeather(data, unit, setLocation) {
-	$("#temperature .value").text(Math.round(data.main.temp));
+	$("#temperature .value").text(Math.round(data.current.temp));
 	$("#temperature .unit").text(window[unit].temperature);
-	$("#description").text(strToTitleCase(data.weather[0].description));
+	$("#description").text(strToTitleCase(data.current.weather[0].description));
 }
 
-openWeatherMap.fetchWeatherByCityAndCountry($("#friend-location").text());
+openWeatherMap.directGeocoding($("#friend-location").text())
+.then(data => {
+	openWeatherMap.fetchWeather(data[0].lat, data[0].lon);
+});
